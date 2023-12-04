173013a579f64af46b2175f96fc34b4eef65f3ce
MylanSe <92014727+MylanSe@users.noreply.github.com>
2023-12-04 16:51:36 +0100

    Modif

---
+++ src/main/java/com/ipi/jva320/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.ipi.jva320.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// HomeController.java :
+@Controller
+public class HomeController {
+    @GetMapping(value = "/")
+    public String home(final ModelMap model) {
+        int count = 25;
+        model.put("nbSalarie", count);
+        return "home";
+    }
+}
