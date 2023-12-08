0e86a3fc4ca3da189e4c579e1af27027ceeaf23b
MylanSe <92014727+MylanSe@users.noreply.github.com>
2023-12-08 12:30:37 +0100

    Creer

--- src/main/java/com/ipi/jva320/controller/SalarieController.java
+++ src/main/java/com/ipi/jva320/controller/SalarieController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.ipi.jva320.exception.SalarieException;
 import com.ipi.jva320.model.SalarieAideADomicile;
 import com.ipi.jva320.service.SalarieAideADomicileService;
 
@@ -29,5 +31,16 @@ public class SalarieController {
         model.put("moisEnCours", sad.getMoisEnCours());
         model.put("moisDebutContrat", sad.getMoisDebutContrat());
         return "detail_Salarie";
+    }
+
+    @GetMapping(value="salaries/aide/new")
+    public String salarieDomicile() throws SalarieException{
+        return "detail_Salarie";
+    }
+
+    @PostMapping(value="salaries/aide/new")
+    public String creerSalarieDomicile(SalarieAideADomicile salarie) throws SalarieException{
+        sas.creerSalarieAideADomicile(salarie);
+        return "redirect:/salaries/detail_Salarie";
     }
 }
